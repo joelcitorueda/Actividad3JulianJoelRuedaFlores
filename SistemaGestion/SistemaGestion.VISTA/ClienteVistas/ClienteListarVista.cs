5950680ccea14fff3e726a14ad97817a43a83e97
joelcitorueda <ruedafloresjulian722@gmail.com>
2024-04-02 08:06:21 -0400

    actividad 3

---
+++ SistemaGestion/SistemaGestion.VISTA/ClienteVistas/ClienteListarVista.cs
@@ -0,0 +1,66 @@
+﻿using SistemaGestion.BSS;
+using SistemaGestion.DAL;
+using SistemaGestion.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGestion.VISTA.ClienteVistas
+{
+	public partial class ClienteListarVista : Form
+	{
+		public ClienteListarVista()
+		{
+			InitializeComponent();
+		}
+		ClienteBss bss = new ClienteBss();
+
+		private void ClienteListarVista_Load(object sender, EventArgs e)
+		{
+			dataGridView1.DataSource = bss.ListarClienteBss();
+		}
+		private void frm_closing(object sender, FormClosingEventArgs e)
+		{
+			this.Show();
+		}
+		private void button2_Click(object sender, EventArgs e)
+		{
+			ClienteInsertarVista fr = new ClienteInsertarVista();
+			this.Hide();
+			fr.FormClosing += frm_closing;
+			if (fr.ShowDialog() == DialogResult.OK)
+			{
+				dataGridView1.DataSource = bss.ListarClienteBss();
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			int IdClienteSeleccionado = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+			ClienteEditarVista fr = new ClienteEditarVista(IdClienteSeleccionado);
+			this.Hide();
+			fr.FormClosing += frm_closing;
+			if (fr.ShowDialog() == DialogResult.OK)
+			{
+				dataGridView1.DataSource = bss.ListarClienteBss();
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			int IdClienteSeleccionado = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+			DialogResult result = MessageBox.Show("Estas Seguro de eliminar este cliente?", "Eliminado", MessageBoxButtons.YesNo);
+			if (result == DialogResult.Yes)
+			{
+				bss.EliminarClienteBss(IdClienteSeleccionado);
+				dataGridView1.DataSource = bss.ListarClienteBss();
+			}
+		}
+	}
+}
